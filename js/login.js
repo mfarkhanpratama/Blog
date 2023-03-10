46c95c2d45e9e7316d635587ca51fa99c40a5682
m fakrhan pratama <farkhan3123@gmail.com>
2023-03-10 08:37:53 +0700

    adding function to take form in login page turn into json

---
+++ js/login.js
@@ -0,0 +1,12 @@
+const submitBtn = document.getElementById("loginButton");
+
+submitBtn.addEventListener("click", () => {
+  // membuat objek FormData untuk mengambil data dari form
+  const formData = new FormData(document.querySelector("form"));
+
+  // mengubah objek FormData ke dalam bentuk JSON
+  const jsonData = JSON.stringify(Object.fromEntries(formData));
+
+  // menampilkan data yang telah diubah ke dalam bentuk JSON
+  console.log(jsonData);
+});
